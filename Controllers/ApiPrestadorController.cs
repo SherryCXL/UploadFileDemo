e1751cdbbd14bf2d41dadcdcede045a658826f77
Xueli Chen <v-xuelc@microsoft.com>
2020-04-23 09:33:43 +0800

    first commit

---
+++ Controllers/ApiPrestadorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo2.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo2.Controllers
+{
+    [ApiVersion("1")]
+    [Route("v{v:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiPrestadorController : ControllerBase
+    {
+        [HttpPost("Upload2")]
+        public async Task<IActionResult> Upload2(FotoViewModel model)
+        {
+            try
+            {
+                if (model == null) return BadRequest("Arquivo não fornecido");
+
+                return Ok(true);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/FotoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo2.Models.ViewModels
+{
+    public class FotoViewModel
+    {
+        public string ParentId { get; set; }
+        public string Imagem { get; set; }
+        public string filename { get; set; }
+    }
+}
